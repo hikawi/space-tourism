46887ea6525eba8cdb5a91354f02173d1882b5fe
tsukimi <128211112+hikawi@users.noreply.github.com>
2024-10-13 03:54:59 +0700

    translated some destination

---
+++ src/components/destinations/DestinationSelect.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import { $destination } from "@/i18n/translations";
+import { useStore } from "@nanostores/vue";
+import { useTemplateRef } from "vue";
+
+const alts = ["moon", "mars", "europa", "titan"];
+const dest = defineModel<number>();
+const tl = useStore($destination);
+
+const destRefs = useTemplateRef<HTMLDivElement[]>("destinations");
+
+// Custom handle key presses since we use radio groups role to accurately style
+// a radio set of buttons, without the native circle thing messing everything up.
+function handleKeyPress(e: KeyboardEvent, idx: number) {
+  let target = idx;
+  switch (e.key) {
+    case "ArrowLeft":
+      target--;
+      if (target < 0) target += 4;
+      destRefs.value?.at(target)?.focus();
+      break;
+    case "ArrowRight":
+      target++;
+      if (target > 0) target -= 4;
+      destRefs.value?.at(target)?.focus();
+      break;
+    case "Enter":
+    case "Space":
+    case " ":
+      destRefs.value?.at(target)?.click();
+      break;
+  }
+}
+</script>
+
+<template>
+  <div
+    class="flex w-full flex-row items-center justify-center gap-8 px-9"
+    role="radiogroup"
+  >
+    <div
+      v-for="(choice, idx) in alts"
+      class="text-preset-8 flex h-8 cursor-pointer items-start border-b-[3px] border-transparent text-center text-base uppercase tracking-[0.125rem]"
+      :class="{
+        'border-white': dest === idx,
+        'text-blue-300 hover:border-b-white hover:border-opacity-50 hover:text-white focus:border-b-white focus:border-opacity-50':
+          dest !== idx,
+      }"
+      role="radio"
+      :aria-checked="dest === idx"
+      @click.prevent="dest = idx"
+      @keydown.prevent="(e) => handleKeyPress(e, idx)"
+      tabindex="0"
+      ref="destinations"
+    >
+      {{ (tl as any)[choice] }}
+    </div>
+  </div>
+</template>
---
+++ src/components/destinations/DestinationSwitcher.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import imageEuropa from "@/assets/destination/image-europa.webp";
+import imageMars from "@/assets/destination/image-mars.webp";
+import imageMoon from "@/assets/destination/image-moon.webp";
+import imageTitan from "@/assets/destination/image-titan.webp";
+import { $destination } from "@/i18n/translations";
+import { useStore } from "@nanostores/vue";
+import { ref } from "vue";
+import DestinationSelect from "./DestinationSelect.vue";
+
+const dest = ref(0);
+const images = [imageMoon, imageMars, imageEuropa, imageTitan];
+const alts = ["moon", "mars", "europa", "titan"];
+
+const tl = useStore($destination);
+</script>
+
+<template>
+  <div class="flex flex-col gap-8">
+    <div class="w-full p-6">
+      <img
+        :src="images[dest].src"
+        :alt="(tl as any)[alts[dest]]"
+        class="mx-auto size-[12rem] object-contain md:size-[24rem] xl:w-[33rem]"
+      />
+    </div>
+
+    <div
+      class="mt-8 flex flex-col items-center justify-center gap-6 text-center xl:items-start xl:text-left"
+    >
+      <DestinationSelect v-model="dest" />
+      <h1
+        class="text-preset-2 text-[3.5rem] uppercase md:text-[5rem] xl:text-[6rem]"
+      >
+        {{ (tl as any)[alts[dest]] }}
+      </h1>
+      <p
+        class="text-preset-9 text-center text-[15px] text-blue-300 md:max-w-[32.125rem] md:text-base xl:max-w-[28rem] xl:text-left xl:text-lg"
+      >
+        {{ (tl as any)[alts[dest] + "Description"] }}
+      </p>
+    </div>
+  </div>
+</template>
